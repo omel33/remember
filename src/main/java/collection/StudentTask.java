6e16b93d9e43ba29ac233f6e4b904b58ccb7b76c
Mykola <omelka3318@gmail.com>
2025-08-25 20:48:04 +0300

    commit

---
+++ src/main/java/collection/StudentTask.java
@@ -0,0 +1,36 @@
+package collection;
+
+import java.util.*;
+
+public class StudentTask {
+    public static void main(String[] args) {
+        List<String> taskList = new ArrayList<>();
+        taskList.add("Create a presentation");
+        taskList.add("Planing meeting");
+        taskList.add("Writing report");
+        taskList.add("Create a presentation");
+        taskList.add("Checking emails");
+        System.out.println("-----------ArrayList--------------");
+        System.out.println(taskList);
+        taskList.remove(1);
+        Set<String> uniqueTasks = new HashSet<>(taskList);
+        System.out.println("-----------HashSet--------------");
+        System.out.println(uniqueTasks);
+        System.out.println("-----------HashSet for each--------------");
+        for (String task : uniqueTasks) {
+            System.out.println(task);
+        }
+        Map<Integer, String> students = new HashMap<>();
+        students.put(1, "John");
+        students.put(2, "Jane");
+        students.put(3, "Bob");
+        System.out.println("-----------HashMap--------------");
+        System.out.println(students);
+        System.out.println("-----------HashMap for each--------------");
+        for (Map.Entry<Integer, String> entry : students.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+    }
+
+}
