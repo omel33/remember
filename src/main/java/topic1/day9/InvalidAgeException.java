3e67e86b6c65f725ee68cb1d077b8277367a9ed1
omel33 <omelka3318@gmail.com>
2025-09-17 16:20:11 +0300

    exept

---
+++ src/main/java/topic1/day9/InvalidAgeException.java
@@ -0,0 +1,7 @@
+package topic1.day9;
+
+public class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
--- src/main/java/topic1/day9/Main2.java
+++ src/main/java/topic1/day9/Main2.java
@@ -3,9 +3,52 @@ package topic1.day9;
 public class Main2 {
     public static void main(String[] args)
     {
+        int val = getElement(new int[]{1, 2, 3}, 4);
+        System.out.println("getElement result = " + val);
+        System.out.println("Operation finished");
+        try {
+            checkAge(17);
+        } catch (InvalidAgeException e) {
+            System.out.println("Denied: " + e.getMessage());;
+        }
 
+        try {
+            System.out.println(div(10, 0));
+        }catch (ArithmeticException  e){
+            System.out.println("Division by zero");
+        }
+        try {
+            System.out.println("10 / 2 = " + div(10, 2));
+        } catch (ArithmeticException e) {
+            System.out.println("Divide error: " + e.getMessage());
+        }
+    }
+    public static int div(int a, int b)
+    {
+        if(b==0)  {
+            throw new ArithmeticException("Division by zero");
 
-        System.out.println(Day.isWeekend(Day.MONDAY));
-        System.out.println(Day.isWeekend(Day.SUNDAY));
+        }
+        return a / b;
+    }
+    public static void checkAge(int age) throws InvalidAgeException
+    {
+        if(age < 18)
+        {
+            throw new InvalidAgeException("Invalid age, age must be >=18");
+        }else {
+            System.out.println("Access granted");
+        }
+    }
+    public static int getElement(int[] arr, int index)
+    {
+        try {
+            return arr[index];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index out of bounds: " + index);
+            return -1;
+        } finally {
+            System.out.println("Operation finished");
+        }
     }
 }
